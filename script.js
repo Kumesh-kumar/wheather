e293146d1616d1756dcf9f9de7dbe9fbbbcd7f27
Kumesh-kumar <kumeshk720@gmail.com>
2026-05-24 14:18:50 +0530

    feat: added get current location

--- script.js
+++ script.js
@@ -1,41 +1,114 @@
-
-
-
-
-// wheather input data 
+// weather input data
 const wheather_search_form = document.querySelector(".wheather_search");
-const wheather_input = document.querySelector(".wheather_input")
+const wheather_input = document.querySelector(".wheather_input");
+const wheather_btn = document.querySelector(".wheather_btn");
 
-// main data in wheather 
-const wheather_city = document.querySelector('.wheather_city');
+// main data in weather
+const wheather_city = document.querySelector(".wheather_city");
 const wheather_date_time = document.querySelector(".wheather_date_time");
 const wheather_forecast = document.querySelector(".wheather_forecast");
 const wheather_icon = document.querySelector(".wheather_icon");
 const wheather_temp = document.querySelector(".wheather_temp");
 const wheather_min = document.querySelector(".wheather_min");
-const wheather_max = document.querySelector(".wheather_max")
+const wheather_max = document.querySelector(".wheather_max");
 
-// wheather_extra_data 
+// weather extra data
 const wheather_feelsLike = document.querySelector(".wheather_feels_like");
-const wheather_humidity = document.querySelector(".wheather_humidity")
-const wheather_wind = document.querySelector(".wheather_wind")
-const wheather_pressure = document.querySelector(".wheather_pressure")
-const card_detail = document.querySelectorAll('.card_detail')
+const wheather_humidity = document.querySelector(".wheather_humidity");
+const wheather_wind = document.querySelector(".wheather_wind");
+const wheather_pressure = document.querySelector(".wheather_pressure");
 
-let city = "mohali"
+const card_detail = document.querySelectorAll(".card_detail");
 
+const API_KEY = "2e4c6c53fb601aa8f62ed73626245f51";
 
-wheather_search_form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    city = wheather_input.value
-    getWheatherData()
-    wheather_input.value = ""
-})
+let city = "mohali";
+
+// ===============================
+// SEARCH WEATHER BY CITY
+// ===============================
+
+wheather_search_form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const inputValue = wheather_input.value.trim();
+
+    if (!inputValue) return;
+
+    await getWheatherData(inputValue);
+
+    wheather_input.value = "";
+
+});
+
+// ===============================
+// GET USER CURRENT LOCATION
+// ===============================
+
+wheather_btn.addEventListener("click", () => {
+
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(
+            async (position) => {
+
+                const lat = position.coords.latitude;
+                const lon = position.coords.longitude;
+
+                const api_url =
+                    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+                try {
+
+                    const response = await fetch(api_url);
+                    const data = await response.json();
+
+                    console.log("Current Location Weather:", data);
+
+                    updateWeatherUI(data);
+
+                } catch (error) {
+
+                    console.log("API is not working", error);
+
+                }
+
+            },
+            (error) => {
+
+                console.log("Location Error:", error);
+
+                alert("Unable to get your location.");
+
+            }
+        );
+
+    } else {
+
+        alert("Geolocation is not supported by this browser.");
+
+    }
+
+});
+
+// ===============================
+// COUNTRY NAME
+// ===============================
+
 const getCountryData = (code) => {
-    return new Intl.DisplayNames([code], { type: "region" }).of(code)
-}
+    return new Intl.DisplayNames(["en"], {
+        type: "region",
+    }).of(code);
+};
+
+// ===============================
+// DATE & TIME FORMAT
+// ===============================
+
 const getDateTimeFormat = (date) => {
-    const curDate = new Date(date * 1000); // convert seconds to milliseconds
+
+    const curDate = new Date(date * 1000);
 
     const options = {
         weekday: "long",
@@ -44,47 +117,103 @@ const getDateTimeFormat = (date) => {
         day: "numeric",
         hour: "numeric",
         minute: "numeric",
+    };
 
+    return new Intl.DateTimeFormat("en-US", options).format(curDate);
+};
 
-    }
+// ===============================
+// GET WEATHER DATA
+// ===============================
 
-    return new Intl.DateTimeFormat('en-US', options).format(curDate)
-}
-const getWheatherData = async () => {
+const getWheatherData = async (cityName) => {
 
-    const api_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2e4c6c53fb601aa8f62ed73626245f51`
+    const api_url =
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+
     try {
-        const response = await fetch(api_url)
-        const data = await response.json()
-        console.log("wheather data", data)
 
+        const response = await fetch(api_url);
+        const data = await response.json();
 
-        card_detail[0].children[0].innerText = "Feels like";
-        card_detail[1].children[0].innerText = "Humidity";
-        card_detail[2].children[0].innerText = "Wind";
-        card_detail[3].children[0].innerText = "Pressure";
+        console.log("Weather Data:", data);
 
-        wheather_city.innerText = `${data.name}, ${getCountryData(data.sys.country)}`
-        wheather_date_time.innerText = `${getDateTimeFormat(data.dt)}`
+        // city not found
+        if (data.cod === "404") {
+            alert("City not found");
+            return;
+        }
 
-        wheather_forecast.innerText = `${data.weather[0].main}`
-        wheather_icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" class="image_adjust"/>`;
-        wheather_temp.innerHTML = `${data.main.temp}&#176`
-        wheather_temp.style.fontSize = "1.5rem"
-        wheather_min.innerHTML = `Min: ${data.main.temp_min}&#176`
-        wheather_max.innerHTML = `Max: ${data.main.temp_max}&#176`
+        updateWeatherUI(data);
 
-        wheather_feelsLike.innerHTML = `${data.main.feels_like}&#176`;
-        wheather_humidity.innerText = `${data.main.humidity} %`
-        wheather_wind.innerText = `${data.wind.speed} m/s`
-        wheather_pressure.innerText = `${data.main.pressure} hPa`
     } catch (error) {
-        console.log("Api is not working", error)
+
+        console.log("API is not working", error);
+
     }
 
+};
 
+// ===============================
+// UPDATE UI
+// ===============================
 
+const updateWeatherUI = (data) => {
 
-}
+    // card headings
+    card_detail[0].children[0].innerText = "Feels Like";
+    card_detail[1].children[0].innerText = "Humidity";
+    card_detail[2].children[0].innerText = "Wind";
+    card_detail[3].children[0].innerText = "Pressure";
 
-document.body.addEventListener("load", getWheatherData())
+    // city name
+    wheather_city.innerText =
+        `${data.name}, ${getCountryData(data.sys.country)}`;
+
+    // date and time
+    wheather_date_time.innerText =
+        `${getDateTimeFormat(data.dt)}`;
+
+    // forecast
+    wheather_forecast.innerText =
+        `${data.weather[0].main}`;
+
+    // icon
+    wheather_icon.innerHTML =
+        `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" class="image_adjust"/>`;
+
+    // temperature
+    wheather_temp.innerHTML =
+        `${Math.round(data.main.temp)}&#176C`;
+
+    wheather_temp.style.fontSize = "1.5rem";
+
+    // min max temp
+    wheather_min.innerHTML =
+        `Min: ${Math.round(data.main.temp_min)}&#176C`;
+
+    wheather_max.innerHTML =
+        `Max: ${Math.round(data.main.temp_max)}&#176C`;
+
+    // extra details
+    wheather_feelsLike.innerHTML =
+        `${Math.round(data.main.feels_like)}&#176C`;
+
+    wheather_humidity.innerText =
+        `${data.main.humidity}%`;
+
+    wheather_wind.innerText =
+        `${data.wind.speed} m/s`;
+
+    wheather_pressure.innerText =
+        `${data.main.pressure} hPa`;
+
+};
+
+// ===============================
+// DEFAULT WEATHER ON PAGE LOAD
+// ===============================
+
+window.addEventListener("load", () => {
+    getWheatherData(city);
+});
